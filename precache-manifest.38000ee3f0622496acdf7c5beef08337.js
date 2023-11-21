f2c47e0205dd9fdb92c8c010ff465a52b93011dc
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2023-11-21 18:50:27 +0900

    Add files via upload

---
+++ precache-manifest.38000ee3f0622496acdf7c5beef08337.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d29446c0724907b64c3",
+    "url": "/static/css/main.50ce5052.chunk.css"
+  },
+  {
+    "revision": "4d29446c0724907b64c3",
+    "url": "/static/js/main.4d29446c.chunk.js"
+  },
+  {
+    "revision": "cf042e081f8a87233510",
+    "url": "/static/js/1.cf042e08.chunk.js"
+  },
+  {
+    "revision": "3e776814da26077ba516",
+    "url": "/static/js/2.3e776814.chunk.js"
+  },
+  {
+    "revision": "69133d183c995e1d60fc",
+    "url": "/static/js/runtime~main.69133d18.js"
+  },
+  {
+    "revision": "f73390b84e0ab5e6c91b12cedcfab8eb",
+    "url": "/static/media/robot.f73390b8.png"
+  },
+  {
+    "revision": "04a1b3713c3db15695fbf33356f7cc9d",
+    "url": "/static/media/MediPhoto.04a1b371.png"
+  },
+  {
+    "revision": "0c80c360b0ebd90ba9ef9ee166ce78ef",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c4b99ebc3b001e56d62074102870d64.js"
+  "/precache-manifest.38000ee3f0622496acdf7c5beef08337.js"
 );
 
 workbox.clientsClaim();
